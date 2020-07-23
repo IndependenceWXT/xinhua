bb295bcaf6866cc5bb7d43d3b635e1730484ff5a
房天生 <fangtiansheng@gmail.com>
2020-07-24 01:14:54 +0800

    :+1:

--- templates/start_requests.py
+++ templates/start_requests.py
@@ -15,29 +15,16 @@ def start_requests(**kwargs):
         cate = each["cate"]
         pages = each["pages"]
         web_site_url = f"http://nnsa.mee.gov.cn/{cate}/"
-    hub_fields = {
-        "web_site": web_site,
-        "web_site_url": web_site_url,
-        "source_type": source_type,
-        "copyright": copyright,
-    }
+        hub_fields = {
+            "web_site": web_site,
+            "web_site_url": web_site_url,
+            "source_type": source_type,
+            "copyright": copyright,
+        }
         for page in range(pages):
             page = f"_{page}" if page else ""
             url = f"http://nnsa.mee.gov.cn/{cate}/index{page}.html"
             yield {"url": url, "page_rule_id": 9944, "hub_fields": hub_fields}
-def start_requests(**kwargs):
-    source_type = 1
-    copyright = 0
-    web_site = "吐鲁番市人民政府"
-
-    web_site_url = "http://www.tlf.gov.cn/zwgk/mlxxlb.jsp?a51t=7&a51p=1&a51c=2000&urltype=tree.TreeTempUrl&wbtreeid=6093"
-    hub_fields = {
-        "web_site": web_site,
-        "web_site_url": web_site_url,
-        "source_type": source_type,
-        "copyright": copyright,
-    }
-    yield {"url": web_site_url, "page_rule_id": 10685, "hub_fields": hub_fields}
 
       
 
