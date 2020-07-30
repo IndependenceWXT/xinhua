4891c002de065c1f4e82c361aebbce7df7c9f5e2
房天生 <fangtiansheng@gmail.com>
2020-07-30 23:31:06 +0800

    :+1:

--- templates/start_requests.py
+++ templates/start_requests.py
@@ -1,30 +1,66 @@
 def start_requests(**kwargs):
     source_type = 1
     copyright = 0
-    web_site = "中融信托"
+    web_site = "中国人寿"
 
-    web_site_url = "http://zritc.com.cn/aboutzr/gsxw/"
+    web_site_url = "http://www.e-chinalife.com/news/gongsixinwen/index.html"
+    hub_fields = {
+        "web_site": web_site,
+        "web_site_url": web_site_url,
+        "source_type": source_type,
+        "copyright": copyright,
+    }
+    for p in range(11):
+        p += 1
+        url = f"http://www.e-chinalife.com/news/gongsixinwen/index.html&curtPage={p}"
+        yield {"url": url, "page_rule_id": None, "hub_fields": hub_fields}
+
+    web_site = "网站名"
+
+    web_site_url = "栏目的链接"
+    hub_fields = {
+        "web_site": web_site,
+        "web_site_url": web_site_url,
+        "source_type": source_type,
+        "copyright": copyright,
+    }
+    for p in range(None):
+        p = f"_{p}" if p else ""
+        url = f"列表页链接然后修改为格式化字符串"
+        yield {"url": url, "page_rule_id": None, "hub_fields": hub_fields}
+
+    yield {"url": web_site_url, "page_rule_id": None, "hub_fields": hub_fields}
+
+    url = ""
+    for p in range(None):
+        p = f"_{p}" if p else ""
+        data = f""
+        yield {
+            "url": url,
+            "page_rule_id": None,
+            "data": data,
+            "method": "POST",
+            "hub_fields": hub_fields,
+        }
+
     cates = [
-        {"cate": "qcbxzs", "pages": 2495},
-        {"cate": "rsbxzs", "pages": 2036},
-        {"cate": "jtbxzs", "pages": 723},
-        {"cate": "lxbxzs", "pages": 660},
-        {"cate": "ylbxzs", "pages": 2223},
-        {"cate": "lcbxzs", "pages": 2064},
+        {"cate": "栏目链接的差异部分", "pages": None},
+        {"cate": "栏目链接的差异部分", "pages": None},
     ]
     for each in cates:
         cate = each["cate"]
         pages = each["pages"]
+        web_site_url = f"栏目的链接格式化"
         hub_fields = {
             "web_site": web_site,
             "web_site_url": web_site_url,
             "source_type": source_type,
             "copyright": copyright,
         }
-        for p in range(1):
+        for p in range(pages):
             p = f"_{p}" if p else ""
-            url = f"http://www.cpic.com.cn/zixun2/{cate}/index{p}.shtml"
-            yield {"url": url, "page_rule_id": 13100, "hub_fields": hub_fields}
+            url = f""
+            yield {"url": url, "page_rule_id": None, "hub_fields": hub_fields}
 
 
 if __name__ == "__main__":
