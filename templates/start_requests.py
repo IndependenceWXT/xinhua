4fba0fbedc08d8586ab7b8bb4f83497baf21bb6e
房天生 <fangtiansheng@gmail.com>
2020-08-03 10:16:44 +0800

    :+1:

--- templates/start_requests.py
+++ templates/start_requests.py
@@ -1,18 +1,18 @@
 def start_requests(**kwargs):
     source_type = 1
     copyright = 0
-    web_site = "网站名"
+    web_site = "新疆生产建设兵团-兵团胡杨网"
 
-    web_site_url = "栏目的链接"
+    web_site_url = "http://www.huyangnet.cn/node_50202.html"
     hub_fields = {
         "web_site": web_site,
         "web_site_url": web_site_url,
         "source_type": source_type,
         "copyright": copyright,
     }
-    for p in range(None):
-        p = f"_{p}" if p else ""
-        url = f"列表页链接然后修改为格式化字符串"
+    for p in range(100):
+        p = f"_{p+1}" if p else ""
+        url = f"http://www.huyangnet.cn/node_50202{p}.html"
         yield {"url": url, "page_rule_id": None, "hub_fields": hub_fields}
 
     yield {"url": web_site_url, "page_rule_id": None, "hub_fields": hub_fields}
