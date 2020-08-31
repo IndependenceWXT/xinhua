d3bea9810be631d5d8ee9112049138ad434412f8
房天生 <fangtiansheng@gmail.com>
2020-08-31 11:39:14 +0800

    Update start_requests.py

--- templates/start_requests.py
+++ templates/start_requests.py
@@ -1,49 +1,52 @@
 def start_requests(**kwargs):
     source_type = 1
-    copyright = 0
-    web_site = "遵义市政府"
+    copyright = 1
+    web_site = "网站名"
 
-    web_site_url = "http://www.zunyi.gov.cn/zwgk/jcxxgk/flfg/zfl_63284/"
+    web_site_url = "栏目的链接"
     hub_fields = {
         "web_site": web_site,
         "web_site_url": web_site_url,
         "source_type": source_type,
         "copyright": copyright,
     }
+    for p in range(None):
+        p = f"_{p}" if p else ""
+        url = f"列表页链接然后修改为格式化字符串"
+        yield {"url": url, "page_rule_id": None, "hub_fields": hub_fields}
+
+    yield {"url": web_site_url, "page_rule_id": None, "hub_fields": hub_fields}
+
+    url = ""
+    for p in range(None):
+        p = f"_{p}" if p else ""
+        data = f""
+        yield {
+            "url": url,
+            "page_rule_id": None,
+            "data": data,
+            "method": "POST",
+            "hub_fields": hub_fields,
+        }
 
     cates = [
-        {"cate": "http://www.zunyi.gov.cn/zwgk/jcxxgk/zcwj/zfl/index", "pages": 1},
-        {"cate": "http://www.zunyi.gov.cn/zwgk/jcxxgk/zcwj/zff_5640672/index", "pages": 2},
-        {"cate": "http://www.zunyi.gov.cn/zwgk/jcxxgk/zcwj/zfbf_5640673/index", "pages": 4},
-        {"cate": "http://www.zunyi.gov.cn/zwgk/jcxxgk/zcwj/zfr_5640674/index", "pages": 6},
-        {"cate": "http://www.zunyi.gov.cn/zwgk/jcxxgk/zcwj/szfqtwj/index", "pages": 2},
-        {"cate": "http://www.zunyi.gov.cn/zwgk/jcxxgk/zcwj/wjbasc/index", "pages": 1},
-        {"cate": "http://www.zunyi.gov.cn/zwgk/jcxxgk/zcwj/wjxgfz/index", "pages": 1},
+        {"cate": "栏目链接的差异部分", "pages": None},
+        {"cate": "栏目链接的差异部分", "pages": None},
     ]
     for each in cates:
         cate = each["cate"]
         pages = each["pages"]
-
+        web_site_url = f"栏目的链接格式化"
+        hub_fields = {
+            "web_site": web_site,
+            "web_site_url": web_site_url,
+            "source_type": source_type,
+            "copyright": copyright,
+        }
         for p in range(pages):
             p = f"_{p}" if p else ""
-            url = cate + p + '.html'
-            yield {"url": url, "page_rule_id": 10999, "hub_fields": hub_fields}
-
-
-    web_site = "遵义市发改委"
-
-    web_site_url = "http://fgw.zunyi.gov.cn/xxgk/zpfl/fgwj/dfxfg/"
-    hub_fields = {
-        "web_site": web_site,
-        "web_site_url": web_site_url,
-        "source_type": source_type,
-        "copyright": copyright,
-    }
-
-    for page in range(17):
-        page = f"_{page}" if page else ""
-        url = f"http://fgw.zunyi.gov.cn/xxgk/zpfl/fgwj/dfxfg/list{page}.html"
-        yield {"url": url, "page_rule_id": 11001, "hub_fields": hub_fields}
+            url = f""
+            yield {"url": url, "page_rule_id": None, "hub_fields": hub_fields}
 
 
 if __name__ == "__main__":
