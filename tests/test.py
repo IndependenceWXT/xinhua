038b5c76824592770a99a767d39fc5585488475d
房天生 <fangtiansheng@gmail.com>
2020-09-23 22:23:08 +0800

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,10 @@
-import logging
-logger = logging.getLogger('__file__')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(ch)
-logger.info('creating an instance of auxiliary_module.Auxiliary')
+class Solution:
+    def paintingPlan(self, n, k):
+        pass
+
 if __name__ == '__main__':
-    logger.info('creating an instance of auxiliary_module.Auxiliary')
+    s = Solution()
+    print(s.paintingPlan(2, 2), 4)
+    print(s.paintingPlan(2, 1), 0)
+    print(s.paintingPlan(2, 4), 1)
+
