b6069ee396f6cebe77a1894c10c6266d83594692
房天生 <fangtiansheng@gmail.com>
2020-10-27 22:27:07 +0800

    :bug: 解析时丢失数据

--- troubleshooting/lajiao.py
+++ troubleshooting/lajiao.py
@@ -1,29 +1,37 @@
-def remove_lines(text):
-    # 预处理：去除非数据块
-    text = "\r\n\r\n".join([i for i in text.split("\r\n\r\n") if "【" not in i and "中国辣椒网诚招全国各产区辣椒报价员" not in i])
-    return text
+def get_date(selector):
+    # 提取日期
+    from datetime import datetime
+
+    date = selector.xpath('.//*[@class="td-time"]/text()[contains(., "-")]')[0]
+    date = date.split("来源")[0].strip().split()[0]
+    date = datetime.strptime(date, "%Y-%m-%d")
+    year = date.year
+    month = date.month
+    day = date.day
+    date = f"{year}/{month}/{day}"
+    return date
 
 
-def remove_empty_lines(text):
-    # 预处理：去除空行
-    while 1:
-        if "\r\n\r\n\u3000" in text:
-            text = text.replace("\r\n\r\n\u3000", "\r\n\u3000")
-        else:
-            return text
+def get_content(selector):
+    # 提取正文文本
+    d = selector.xpath('.//*[@id="content"]')[0]
+    content = d.xpath("string(.)")
+    return content
 
 
-def split_places(text):
-    # 分割数据块
-    return text.split("\r\n\r\n")
+def fill(array):
+    # 用前面的非空值填充后面的空值
+    tmp = ""
+    for i, each in enumerate(array):
+        if each:
+            tmp = each
+        # None值不动
+        elif each == "":
+            array[i] = tmp
+    return array
 
 
-def remove_contact(text):
-    # 去除联系方式
-    return "\r\n\r\n".join(["\r\n".join(i.split("\r\n")[:-2]) for i in text.split("\r\n\r\n")])
-
-
-def extract_price(detail):
+def parse_price(detail):
     # 解析价格详情
     import re
     name = detail.split("：")[0]
@@ -35,7 +43,10 @@ def extract_price(detail):
             unit = res[0]
     p = re.compile(r'(\d+\.\d+|\d+)')
     res = p.findall(price)
-    low = res[0]
+    if res:
+        low = res[0]
+    else:
+        return
     high = res[-1] if len(res)>1 else low
     p = re.compile(r'[\(（](.*?)[\)）]')
     level = p.findall(detail)
@@ -48,49 +59,48 @@ def extract_price(detail):
     }
     return item
 
-def extract_2_level(place, tops, details):
-    # 解析两层数据
-    items = []
-    top = None
-    for line in place.split("\r\n"):
-        if line.strip() in tops:
-            top = line.strip()
+
+def extract(content):
+    # 提取结构
+    data = []
+    # 表格化
+    table = [
+        [each for i, each in enumerate(line.split("\u3000"))]
+        for line in content.split("\r\n")
+        if "：" in line
+    ]
+    # 价格信息
+    width = max(len(i) for i in table)
+    colums = []
+    for c in range(width):
+        # width=3, c: 0, 1
+        # 最后一列不需要填充空值
+        if c < width - 1:
+            colum = fill([i[c] if len(i)>c else None for i in table])
+            colums.append(colum)
         else:
-            detail = line.strip()
-            if "备注" in detail or "：" not in detail:
-                continue
-            item = extract_price(detail)
-            item["place"] = top.replace("：", "")
-            item["source"] = ""
-            items.append(item)
+            colums.append([i[c] if len(i)>c else None for i in table])
+    table = list(zip(*colums))
+    data = [each for each in table if None not in each]
+    items = []
+    for each in data:
+        place = each[0]
+        detail = each[-1]
+        source = each[-2] if width >2 else ""
+        item = parse_price(detail)
+        if not item:
+            continue
+        item["place"] = place.replace("：", "")
+        item["source"] = source.replace("：", "")
+        items.append(item)
     return items
 
 
-def extract_3_level(place, tops, subs, details):
-    # 解析三层数据
-    items = []
-    top = None
-    sub = None
-    for line in place.split("\r\n"):
-        if line.strip() in tops:
-            top = line.strip()
-        elif line.strip() in subs:
-            sub = line.strip()
-        else:
-            detail = line.strip()
-            if "备注" in detail or "：" not in detail:
-                continue
-            item = extract_price(detail)
-            item["place"] = top.replace("：", "")
-            item["source"] = sub.replace("：", "")
-            items.append(item)
+def normalize(date, items):
+    # 数据结构化
+    for item in items:
+        item["date"] = date
     return items
-
-def extract(place, tops, subs, details):
-    if details:
-        return extract_3_level(place, tops, subs, details)
-    else:
-        return extract_2_level(place, tops, subs)
 
 
 def process(text):
@@ -98,41 +108,31 @@ def process(text):
     from datetime import datetime
     import json
 
-    s = etree.HTML(text)
-    # 提取文字保留换行和空格
-    res = []
-    date = s.xpath('.//*[@class="td-time"]/text()[contains(., "-")]')[0]
-    date = date.split("来源")[0].strip().split()[0]
-    date = datetime.strptime(date, "%Y-%m-%d")
-    year = date.year
-    month = date.month
-    day = date.day
-    date = f"{year}/{month}/{day}"
-
-    d = s.xpath('.//*[@id="content"]')
-    text = d[0].xpath('string(.)').strip()
-    normalizers = [remove_lines, remove_empty_lines]
-
-    for each in normalizers:
-        text = each(text)
-
-    places = split_places(text)
-    for place in places:
-        place = remove_contact(place)
-        tops = [i for i in place.strip().split("\r\n") if (not i.startswith("\u3000")) and i]
-        subs = [i for i in place.strip().split("\r\n\u3000") if (not i.startswith("\u3000")) and i and (i not in tops) and "：" in i]
-        details = [i for i in place.strip().split("\r\n\u3000\u3000") if (not i.startswith("\u3000")) and i and (i not in tops or i not in subs) and "\r\n" not in i]
-        data = extract(place, tops, subs, details)
-        res.extend(data)
     result = []
-    for i in res:
-        i["date"] = date
-        result.append(i)
-    return json.dumps(result, indent=4, ensure_ascii=False)
+    selector = etree.HTML(text)
+    date = get_date(selector)
+    content = get_content(selector)
 
-if __name__ == '__main__':
+    items = extract(content)
+
+    result = normalize(date, items)
+
+    return json.dumps(result)
+
+
+def main(url):
     import requests
-    url = "http://www.e658.cn/jg/show-htm-itemid-91658.html"
+    import json
+
     res = requests.get(url)
-    print(process(res.text))
-    
+    result = process(res.text)
+    data = json.loads(result)
+    from pprint import pprint
+    pprint(data)
+    print(len(data))
+
+if __name__ == "__main__":
+    import fire
+
+    fire.Fire(main)
+
