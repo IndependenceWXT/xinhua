efacabe21a447872d183132cf505b1dc2cafdcb9
房天生 <fangtiansheng@gmail.com>
2020-11-12 22:48:59 +0800

    :+1:

---
+++ tests/response_sql.py
@@ -0,0 +1,34 @@
+import requests
+
+
+def field(url):
+    headers = {
+        "Connection": "keep-alive",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "DNT": "1",
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4315.4 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "http://www.chinamoney.com.cn",
+        "Referer": "http://www.chinamoney.com.cn/chinese/mtmoncjgl/",
+        "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    }
+    response = requests.get(url)
+    data = response.json()
+    records = data["records"]
+    fields = records[0].keys()
+    return list(fields)
+
+
+if __name__ == '__main__':
+    url = "http://www.chinamoney.com.cn/ags/ms/cm-u-dlrp/IblDlyBltn?lang=cn&indexType=markVOList&searchDate=2020-11-10"
+    fields = field(url)
+    lines = []
+    for each in fields:
+        line = f"  `{each}` varchar(255) DEFAULT NULL COMMENT '{each}',"
+        lines.append(line)
+    res = "\n".join(lines)
+    print(res)
+    
